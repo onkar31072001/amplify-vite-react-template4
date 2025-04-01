be59c7003bf21125441e3e67f98d965207cbcc03
onkar31072001 <onkarbhukan31@gmail.com>
2025-04-01 15:36:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { useEffect, useState } from 'react';
 import { generateClient } from 'aws-amplify/data';
+import type { Schema } from "../amplify/data/resource";
 
+// Generate the client
 const client = generateClient<Schema>();
 
 function App() {
@@ -11,14 +11,27 @@ function App() {
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+  // Fetch todos using a subscription
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    // Cleanup subscription on component unmount
+    return () => subscription.unsubscribe();
   }, []);
 
+  // Create new todo
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
+  }
+
+  // Delete a todo
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
 
   return (
@@ -27,26 +40,9 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}
-            function App() {
-  // ...
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
-  return (
-    <main>
-       {/* ... */}
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map(todo => 
-            <li
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.content}
           </li>
-                   </li>)}
         ))}
       </ul>
       <div>
@@ -56,9 +52,10 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
-            <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
 
 export default App;
+
